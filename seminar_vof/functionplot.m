beb2b29f7c725da1b03678b114999ed27ad256c6
David <david@THINKPADLNX>
2018-07-16 17:31:37 +0200

    penalizer function plots

--- seminar_vof/functionplot.m
+++ seminar_vof/functionplot.m
@@ -1,29 +1,41 @@
 clear;
 clc;
 
-chrb = @(x,eps) 2*(eps^2)*sqrt(1+(x^2)/(eps^2))-2*(eps^2)+1;
+chrb = @(x,eps) 2*(eps^2)*sqrt(1+(x^2)/(eps^2));%-2*(eps^2)+1;
 chrbp = @(x,eps) 1/sqrt(1+(x^2)/(eps^2));
 prml = @(x,eps) eps^2*log(1+(x^2)/(eps^2));
 prmlp = @(x,eps) 1/(1+(x^2)/(eps^2));
 fnc1 = @(x) chrb(x,1);
 fnp = @(x) prml(x,1);
 fna = @(x) log(abs(x)+1);
+fnl = @(x) 2*abs(x)
 fnq = @(x) x*x;
+fnlog = @(x) 2*log(x)
 x = linspace(-4,4,100);
-y1 = arrayfun(fnc1,x);
-y2 = arrayfun(fna,x);
+y1 = arrayfun(fnp,x);
+y2 = arrayfun(fnlog,x);
 y3 = arrayfun(fnq,x);
 
 hold on;
 p1 = plot(x,y1);
-%p3 = plot(x,y2,'--');
+p3 = plot(x,y2,'--');
 p4 = plot(x,y3,'--');
-axis([-inf inf 0 5]);
+axis([0 3 0 4]);
+%axis([0 3 2 6]);
 axis equal
 set(p1, 'Linewidth', 2.5);
 %set(p2, 'Linewidth', 2.5);
-%set(p3, 'Linewidth', 1.3);
+set(p3, 'Linewidth', 1.3);
 set(p4, 'Linewidth', 1.3);
+
+title("PeronaMalik")
+%title("Charbonnier")
+ylabel('y')
+set(get(gca,'ylabel'),'rotation',0)
+xlabel("s")
+print '-S300,400' -deps peronamalik.eps
+
+
 
 
 %T = 1;
---
+++ seminar_vof/paper/images/fig2eps.m
@@ -0,0 +1,6 @@
+ylim([-2 2])
+axis equal
+xlim([-2 2])
+ylabel("y")
+xlabel("x")
+print "-S300,300" -deps filename.eps
